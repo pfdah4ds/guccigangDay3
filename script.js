c48bee901ecccbd1bc9747c2989b9c029b91dd72
pfdah <pratikdahal700@gmail.com>
2021-06-03 13:41:17 +0545

    Switch

---
+++ script.js
@@ -0,0 +1,16 @@
+//Switch statement
+day = 'Saturday'
+switch(day){
+    case 'Sunday':
+        console.log('Mauja hi mauja');
+        break;
+    case 'Monday':
+        console.log('Commit Bad');
+        break;
+    case 'Saturday':
+        console.log('Commit Bad');
+        break;    
+    default:
+        console.log('Bitch WTF');
+        break;    
+}
