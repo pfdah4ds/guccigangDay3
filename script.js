eb470a60ee386c22369f23d7cccaab4c75840565
pfdah <pratikdahal700@gmail.com>
2021-06-03 14:03:40 +0545

    Functions

--- script.js
+++ script.js
@@ -1,16 +1,75 @@
 //Switch statement
-day = 'Saturday'
+day = 'Paturday'
 switch(day){
     case 'Sunday':
         console.log('Mauja hi mauja');
         break;
-    case 'Monday':
+    case 'Monday','Saturday':
         console.log('Commit Bad');
         break;
-    case 'Saturday':
-        console.log('Commit Bad');
+    case 'Paturday':
+        console.log('Vomit Mad');
         break;    
     default:
         console.log('Bitch WTF');
         break;    
 }
+//Function with function keyword
+
+//No argument no return
+function printHello(){
+    console.log('Hello ducks!');
+}
+printHello();
+printHello();
+printHello();
+printHello();
+
+//Argument Return
+
+function haha(a,b){
+    return a+b;
+}
+console.log(haha(4,6));
+console.log(haha(6,5));
+console.log(haha(8,6));
+
+//Yes Argument No Return
+
+function haha2(a,b){
+    console.log(`The sum is ${a+b}`);
+}
+
+haha2(4,5);
+haha2(5,6);
+haha2(0,1);
+
+
+//No argument return
+console.log(getPI());
+function getPI(){
+    return 3.14;
+}
+l = getPI();
+console.log(l);
+console.log(getPI());
+console.log(getPI());
+
+
+//Function expression
+const quadrilateraleqn = function(a,b,c,x){
+    return a*x*x+b*x+c;
+};
+console.log(quadrilateraleqn(2,3,4,6));
+
+//arrow function
+const sumwithArr = (a,b) =>{
+    console.log(a+b);
+}
+a = 44;
+b = 66;
+sumwithArr(a,b);
+
+//ShorterArrow
+const sumwithArr2 = (a,b) => a+b;
+sumwithArr2(b,a);
